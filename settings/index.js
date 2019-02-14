ab9df7f044364d40f2f1ae83ab8d435444bee53d
Hasan Karahan <hasan.karahan@blackhan.com>
2019-02-14 17:20:50 +0300

    App: config w/persistence

--- settings/index.js
+++ settings/index.js
@@ -1,11 +1,25 @@
-const { readdirSync: readdir } = require('fs');
-const { merge } = require('lodash');
+const { readdirSync } = require('fs');
+const { unlinkSync } = require('fs');
+const { writeFile } = require('fs');
 
-const { remote } = require('electron');
-console.assert(remote || !remote);
-const { argv } = remote ? remote.process : process;
+const { merge } = require('lodash');
+const { join } = require('path');
+
+const process = () => {
+    try {
+        const { remote } = require('electron');
+        if (remote) {
+            return remote.process;
+        }
+        return require('process');
+    } catch (ex) {
+        return require('process');
+    }
+};
+
+const { argv } = process();
 console.assert(argv);
-const { env } = remote ? remote.process : process;
+const { env } = process();
 console.assert(env);
 
 const yargs = require('yargs')
@@ -16,8 +30,18 @@ const yargs = require('yargs')
 const args = yargs.parse(argv.slice(1));
 console.assert(args);
 
-const json_list = readdir(__dirname).filter((name) => {
-    return name.match(/^([0-9]+)-(\w+)\.json$/);
+const apx_config_del = JSON.parse(env['APX_CONFIG_DEL'] || '0');
+const apx_config_set = JSON.parse(env['APX_CONFIG_SET'] || '0');
+const apx_config_tpl = /^([0-9]{3})-(\w+)\.json$/;
+const apx_config_999 = '999-custom.json';
+
+if (apx_config_set || apx_config_del) {
+    const path_to = join(__dirname, apx_config_999);
+    try { unlinkSync(path_to); } catch (ex) { }
+}
+
+const json_list = readdirSync(__dirname).filter((name) => {
+    return apx_config_tpl.test(name);
 }).map((name) => {
     const arr = (a) => a instanceof Array;
     const obj = (o) => o instanceof Object;
@@ -25,7 +49,7 @@ const json_list = readdir(__dirname).filter((name) => {
         ? `${p}_${k}` : k).toUpperCase();
     const value = (p, v) => env[p] !== undefined
         ? JSON.parse(env[p]) : v;
-    const resolve = (json, p = "") => {
+    const resolve = (json, p = 'APX') => {
         if (p && value(p) !== undefined) {
             return value(p);
         }
@@ -39,6 +63,18 @@ const json_list = readdir(__dirname).filter((name) => {
         }
         return value(p, json);
     }
-    return resolve(require(`./${name}`));
+    return resolve(
+        require(`./${name}`)
+    );
 });
+
+if (apx_config_set) {
+    const data = new Uint8Array(Buffer.from(
+        JSON.stringify(merge(...json_list, args), null, 4)
+    ));
+    writeFile(join(__dirname, apx_config_999), data, (e) => {
+        if (e) { console.error(e); }
+    });
+}
+
 module.exports = merge(...json_list, args);
