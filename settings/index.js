97b858b8a5feb5acb9b521fea620a3b16c72bd5b
Hasan Karahan <hasan.karahan@blackhan.com>
2019-02-16 18:45:36 +0300

    App: refactored settings

--- settings/index.js
+++ settings/index.js
@@ -1,3 +1,5 @@
+const { walk } = require('./walk');
+
 const { readdirSync } = require('fs');
 const { unlinkSync } = require('fs');
 const { writeFile } = require('fs');
@@ -30,51 +32,38 @@ const yargs = require('yargs')
 const args = yargs.parse(argv.slice(1));
 console.assert(args);
 
-const apx_config_del = JSON.parse(env['APX_CONFIG_DEL'] || '0');
-const apx_config_set = JSON.parse(env['APX_CONFIG_SET'] || '0');
-const apx_config_tpl = /^([0-9]{3})-(\w+)\.json$/;
-const apx_config_999 = '999-custom.json';
+const settings_dbg = JSON.parse(env['SETTINGS_DBG'] || '0');
+const settings_del = JSON.parse(env['SETTINGS_DEL'] || '0');
+const settings_set = JSON.parse(env['SETTINGS_SET'] || '0');
+const settings_tpl = /^([0-9]{3})-(\w+)\.json$/;
+const settings_999 = '999-custom.json';
 
-if (apx_config_set || apx_config_del) {
-    const path_to = join(__dirname, apx_config_999);
+if (settings_set || settings_del) {
+    const path_to = join(__dirname, settings_999);
     try { unlinkSync(path_to); } catch (ex) { }
 }
-
 const json_list = readdirSync(__dirname).filter((name) => {
-    return apx_config_tpl.test(name);
+    return settings_tpl.test(name);
 }).map((name) => {
-    const arr = (a) => a instanceof Array;
-    const obj = (o) => o instanceof Object;
-    const key = (p, k) => (p
-        ? `${p}_${k}` : k).toUpperCase();
-    const value = (p, v) => env[p] !== undefined
-        ? JSON.parse(env[p]) : v;
-    const resolve = (json, p = 'APX') => {
-        if (p && value(p) !== undefined) {
-            return value(p);
-        }
-        if (obj(json)) {
-            return Object.entries(json)
-                .map(([k, v]) =>
-                    [k, resolve(v, key(p, k))])
-                .reduce((acc, [k, v]) => {
-                    acc[k] = v; return acc;
-                }, arr(json) ? [] : {});
-        }
-        return value(p, json);
-    }
-    return resolve(
-        require(`./${name}`)
-    );
+    return walk(require(`./${name}`), 'APX')({
+        key: (p, k) => `${p}_${k}`.toUpperCase(),
+        value: (p, v) => env[p] !== undefined
+            ? JSON.parse(env[p]) : v
+    });
 });
-
-if (apx_config_set) {
+if (settings_set) {
     const data = new Uint8Array(Buffer.from(
         JSON.stringify(merge(...json_list, args), null, 4)
     ));
-    writeFile(join(__dirname, apx_config_999), data, (e) => {
+    writeFile(join(__dirname, settings_999), data, (e) => {
         if (e) { console.error(e); }
     });
 }
-
-module.exports = merge(...json_list, args);
+const settings = merge(...json_list, args);
+if (settings_dbg) {
+    walk(settings, 'APX')({
+        key: (p, k) => `${p}_${k}`.toUpperCase(),
+        value: (p, k) => console.debug(`${p}=${k}`)
+    });
+}
+module.exports = settings;
